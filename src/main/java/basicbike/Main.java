8e02a43796b3c1c6bef3895edaeb5c7590697f4e
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-04-26 21:03:42 +0700

    feat: Add closing connection when something happen

--- src/main/java/basicbike/Main.java
+++ src/main/java/basicbike/Main.java
@@ -8,17 +8,20 @@ import com.j256.ormlite.support.ConnectionSource;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        // TODO: Use DaoFactory instead
+    public static void main(String[] args) throws Exception {
         System.out.println("And here we are!");
+        ConnectionSource connectionSource = null;
         try {
-            ConnectionSource connectionSource = new JdbcConnectionSource("jdbc:sqlite:database.db");
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:database.db");
             DaoFactory daoFactory = new DaoFactory(connectionSource);
             MainGui mainGui = new MainGui(daoFactory);
             mainGui.start();
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            // If something bad happens, the program must close the connnection.
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
         }
 
     }
