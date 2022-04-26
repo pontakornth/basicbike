fe5b60609e4f9a46e5210262536f565c70bce15a
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-04-26 17:03:53 +0700

    test: Add testing for Dao

--- src/main/java/basicbike/Main.java
+++ src/main/java/basicbike/Main.java
@@ -1,7 +1,38 @@
 package basicbike;
 
+import basicbike.dao.BikeDao;
+import basicbike.dao.BikeDaoImpl;
+import basicbike.dao.BikeItemDao;
+import basicbike.dao.BikeItemDaoImpl;
+import basicbike.model.Bike;
+import basicbike.model.BikeItem;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.sql.SQLException;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+        // TODO: Use DaoFactory instead
         System.out.println("And here we are!");
+        try {
+            ConnectionSource connectionSource = new JdbcConnectionSource("jdbc:sqlite:database.db");
+            BikeDao bikeDao = new BikeDaoImpl(connectionSource);
+            Bike newBike = new Bike("LA 120", "Mountain", 12.5, 50);
+            bikeDao.createOrUpdate(newBike);
+            for (Bike bike: bikeDao.queryForAll()) {
+                System.out.println(bike.getModel());
+            }
+            BikeItem newBikeItem = new BikeItem(newBike, "A2090");
+            BikeItemDao bikeItemDao = new BikeItemDaoImpl(connectionSource);
+            bikeItemDao.createOrUpdate(newBikeItem);
+            bikeItemDao.rent(newBikeItem, "1234", new Date());
+            System.out.println("The bike is already rented");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
