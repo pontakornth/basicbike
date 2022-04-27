55c77f095581e37035af24aacf4406e03eed4964
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-04-27 11:50:32 +0700

    feat: Add renting dialog

--- src/main/java/basicbike/gui/MainGui.java
+++ src/main/java/basicbike/gui/MainGui.java
@@ -4,8 +4,11 @@ import basicbike.dao.DaoFactory;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumnModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -14,6 +17,7 @@ public class MainGui extends JFrame {
     private final DaoFactory daoFactory;
     private final FilterPanel filterPanel;
     private final ResultTable resultTable;
+    private final ActionPanel actionPanel;
 
     private static class FilterPanel extends JPanel {
         public final JLabel modelInputLabel;
@@ -36,22 +40,53 @@ public class MainGui extends JFrame {
         public final JTable table;
         public ResultTable() {
             // TODO: Make it actually work.
-            String[] columnName = new String[]{"Bike ID", "Model", "Type", "Size", "Status", "Action"};
+            String[] columnName = new String[]{"Bike ID", "Model", "Type", "Size", "Status"};
             // TODO: Use data from database
-            String[][] data = new String[][]{
-                    {"A1234", "LA Mountain Kun", "Mountain", "Available", "Rent"},
-                    {"A1234", "LA Mountain Kun", "Mountain", "Available", "Rent"},
-                    {"A1234", "LA Mountain Kun", "Mountain", "Available", "Rent"},
-                    {"A1234", "LA Mountain Kun", "Mountain", "Available", "Rent"},
+            Object[][] data = new Object[][]{
+                    {"A1234", "LA Mountain Kun", "Mountain", 20, "Available"},
+                    {"A1234", "LA Mountain Kun", "Mountain", 20, "Available"},
+                    {"A1234", "LA Mountain Kun", "Mountain", 20, "Available"},
+                    {"A1234", "LA Mountain Kun", "Mountain", 20, "Available"},
             };
             table = new JTable();
             DefaultTableModel model = new DefaultTableModel(data, columnName);
             table.setModel(model);
             table.getColumn("Model").setPreferredWidth(200);
-//            table.setFillsViewportHeight(true);
-            table.setCellEditor(null);
+            table.setFillsViewportHeight(true);
+            // The table is not editable because it is outside of scope.
+            table.setDefaultEditor(Object.class, null);
             setLayout(new BorderLayout());
             add(new JScrollPane(table), BorderLayout.SOUTH);
+        }
+    }
+
+    private static class ActionPanel extends JPanel {
+        private final JButton rentButton;
+
+        public ActionPanel() {
+            // TODO: Add button action
+            rentButton = new JButton("Rent");
+            rentButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // TODO: Add checking if the bike is already rented.
+                    JPanel panel = new JPanel(new GridLayout(3,2));
+                    JLabel idLabel = new JLabel("National ID or Passport");
+                    JTextField idInput = new JTextField();
+                    idInput.setColumns(20);
+                    JLabel timeLabel = new JLabel("Time");
+                    JTextField timeInput = new JTextField();
+                    timeInput.setColumns(20);
+                    panel.add(idLabel);
+                    panel.add(idInput);
+                    panel.add(timeLabel);
+                    panel.add(timeInput);
+                    int result = JOptionPane.showConfirmDialog(null, panel, "Rent",
+                            JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+                }
+            });
+            setLayout(new FlowLayout());
+            add(rentButton);
         }
     }
 
@@ -60,8 +95,10 @@ public class MainGui extends JFrame {
         this.daoFactory = daoFactory;
         this.filterPanel = new FilterPanel();
         this.resultTable = new ResultTable();
+        this.actionPanel = new ActionPanel();
         add(filterPanel, BorderLayout.NORTH);
         add(resultTable, BorderLayout.CENTER);
+        add(actionPanel, BorderLayout.SOUTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             @Override
