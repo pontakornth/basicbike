453b0969793460342f890f4dd3bef7adebef2f5a
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-04-28 10:50:30 +0700

    feat: Allow configuration

--- src/main/java/basicbike/Main.java
+++ src/main/java/basicbike/Main.java
@@ -5,14 +5,21 @@ import basicbike.gui.MainGui;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("And here we are!");
         ConnectionSource connectionSource = null;
+        InputStream inputStream;
         try {
-            // TODO: Allow connection string to change
-            connectionSource = new JdbcConnectionSource("jdbc:sqlite:database.db");
+            String configFileLocation = "config.properties";
+            Properties p = new Properties();
+            p.load(Main.class.getClassLoader().getResourceAsStream(configFileLocation));
+
+            String connectionString = String.format("jdbc:sqlite:%s", p.getProperty("databaseFile"));
+            connectionSource = new JdbcConnectionSource(connectionString);
             DaoFactory daoFactory = new DaoFactory(connectionSource);
             MainGui mainGui = new MainGui(daoFactory);
             mainGui.start();
